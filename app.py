858a742794156272deead0327202f0eb851b5476
Komal Patil <kp3807049@gmail.com>
2026-02-25 10:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,122 +1,3 @@
-# import streamlit as st
-# import numpy as np
-# import pickle
-# import os
-
-# # ===============================
-# # Load model
-# # ===============================
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# model_path = os.path.join(BASE_DIR, "model.pkl")
-
-# with open(model_path, "rb") as f:
-#     model = pickle.load(f)
-
-
-# # ===============================
-# # Page config
-# # ===============================
-# # st.set_page_config(
-# #     page_title="Restaurant Popularity Predictor",
-# #     page_icon="🍽️",
-# #     layout="centered"
-# # )
-
-# # ===============================
-# # Custom Glass UI Style
-# # ===============================
-# st.markdown("""
-# <style>
-# .stApp {
-#     background: linear-gradient(135deg, #1f4037, #99f2c8);
-# }
-
-# .card {
-#     background: rgba(255,255,255,0.15);
-#     backdrop-filter: blur(12px);
-#     border-radius: 20px;
-#     padding: 25px;
-#     box-shadow: 0 8px 32px rgba(0,0,0,0.25);
-# }
-
-# .title {
-#     text-align:center;
-#     font-size:32px;
-#     font-weight:bold;
-#     color:white;
-#     margin-bottom:20px;
-# }
-
-# .pred {
-#     font-size:26px;
-#     font-weight:bold;
-#     text-align:center;
-#     color:#00ffcc;
-#     margin-top:20px;
-# }
-# </style>
-# """, unsafe_allow_html=True)
-
-# # ===============================
-# # Title
-# # ===============================
-# st.markdown(
-#     '<div class="title">🍽️ Restaurant Popularity Predictor</div>',
-#     unsafe_allow_html=True
-# )
-
-# # ===============================
-# # Correct Location Encoding
-# # ===============================
-# location_map = {
-#     "Indore": 7,
-#     "Delhi": 4,
-#     "Mumbai": 10,
-#     "Surat": 14,
-#     "Ahmedabad": 0,
-#     "Hyderabad": 6,
-#     "Bhopal": 2,
-#     "Jaipur": 8,
-#     "Goa": 5,
-#     "Chennai": 3,
-#     "Nagpur": 11,
-#     "Bangalore": 1,
-#     "Pune": 13,
-#     "Kochi": 9,
-#     "Nashik": 12
-# }
-
-# # ===============================
-# # UI
-# # ===============================
-# st.set_page_config(page_title="Restaurant Popularity Predictor", page_icon="🍽️")
-# # st.title("🍽️ Restaurant Popularity Predictor")
-
-# location_name = st.selectbox("Select Location", list(location_map.keys()))
-# cost = st.number_input("Average Cost", value=300)
-# rating = st.number_input("Rating", value=3.5)
-# votes = st.number_input("Votes", value=100)
-# reviews = st.number_input("Reviews Count", value=50)
-# Delivery_Time= st.number_input("Delivery Time", value=0.0)
-
-# # ===============================
-# # Prediction
-# # ===============================
-# if st.button("Predict Popularity"):
-#     loc_encoded = location_map[location_name]
-    
-#     features = np.array([[loc_encoded, cost, rating, votes, reviews, Delivery_Time]])
-    
-#     pred = model.predict(features)[0]
-    
-#     st.success(f"Predicted Popularity: {pred}")
-
-
-
-
-
-
-
 
 import streamlit as st
 import numpy as np
@@ -189,21 +70,9 @@ st.markdown(
 # ===============================
 with st.container():
     location_map = {
-        "Indore": 7,
-        "Delhi": 4,
-        "Mumbai": 10,
-        "Surat": 14,
-        "Ahmedabad": 0,
-        "Hyderabad": 6,
-        "Bhopal": 2,
-        "Jaipur": 8,
-        "Goa": 5,
-        "Chennai": 3,
-        "Nagpur": 11,
-        "Bangalore": 1,
-        "Pune": 13,
-        "Kochi": 9,
-        "Nashik": 12
+        'Indore': 0, 'Delhi': 1, 'Mumbai': 2, 'Surat': 3, 'Ahmedabad': 4, 'Hyderabad': 5,
+       'Bhopal': 6, 'Jaipur': 7, 'Goa': 8, 'Chennai': 9, 'Nagpur': 10, 'Bangalore': 11,
+       'Pune': 12, 'Kochi': 13, 'Nashik': 14
     }
 
     col1, col2 = st.columns(2)
@@ -235,4 +104,5 @@ with st.container():
         )
 
     st.markdown('</div>', unsafe_allow_html=True)
+
 
